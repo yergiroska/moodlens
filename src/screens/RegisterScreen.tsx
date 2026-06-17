0d143ca08fc99ea4d6ba3045aba2b93addd40813
Yergiroska Aguirre <yergiroska77@gmail.com>
2026-06-17 08:55:07 +0200

    fix: add text color to register password fields

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -106,6 +106,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         marginBottom: 12,
         backgroundColor: '#fafafa',
+        color: '#000000',
     },
     button: {
         width: '100%',
